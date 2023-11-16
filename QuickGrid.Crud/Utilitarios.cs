a04bb1750ff932034bd65fc62ff55a8c7782b473
Carlos Alberto de Almeida Junior <carlos.dealmeiida@gmail.com>
2023-11-16 18:02:46 -0300

    Adicionando o .NET8 para geração no pacote

---
+++ QuickGrid.Crud/Utilitarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quick_crud
+{
+    public static class Utilitarios
+    {
+        public static int GetVersionNET()
+        {
+            #if NET7_0
+                return 7;
+            #elif NET8_0
+                return 8;
+            #endif
+        }
+    }
+}
